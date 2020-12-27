c4adcb344452a0c4f1af086e1f61a8584fd0dfa7
Guyzaidman <guyzaidm@gmail.com>
2020-12-27 13:42:52 +0200

    started implimenting game mode, changed communication to struct module

--- Client.py
+++ Client.py
@@ -1,16 +1,18 @@
 import socket
+import struct
 
 class Client:
     UDP_PORT = 13117
     offer_size = 56
-    magic_cookie = '11111110111011011011111011101111'
-    m_type = '00000010'
+    magic_cookie = 0xfeedbeef
+    m_type = 0x2
 
     def __init__(self, team_name):
         self.team_name = team_name
 
         self.udp_sock = socket.socket(socket.AF_INET, # Internet
                                         socket.SOCK_DGRAM) # UDP
+        self.udp_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.udp_sock.bind(('', self.UDP_PORT))
 
         self.tcp_sock = socket.socket(socket.AF_INET, # Internet
@@ -26,21 +28,35 @@ class Client:
             
             print("recieved offer from {}, attempting to connect...".format(addr[0]))
 
-            if self.check_data(data):
-                return data.decode('utf-8'), addr
+            try:
+                if self.check_data(data):
+                    # return data.decode('utf-8'), addr
+                    return struct.unpack('IbH',data), addr
+            except:
+                print('check_data went wrong')
+                continue
 
     def check_data(self, data):
-        decoded_data = data.decode('utf-8')
-        if len(decoded_data) != self.offer_size or decoded_data[0:32] != self.magic_cookie or decoded_data[32:40] != self.m_type:
+        decoded_data = struct.unpack('IbH',data)
+        if len(decoded_data) != 3 or decoded_data[0] != self.magic_cookie or decoded_data[1] != self.m_type:
             return False
         
         return True
 
     def initiate_tcp_connection(self, server_ip, server_tcp_port):
         print(server_ip)
-        self.tcp_sock.connect(('172.1.0.61', server_tcp_port))
+        self.tcp_sock.connect((server_ip, server_tcp_port))
 
         self.tcp_sock.send(bytes(self.team_name+'\n', 'utf-8'))
+
+    def receive_message(self):
+        while True:
+            message = self.tcp_sock.recv(1024).decode('utf-8')
+            print(message)
+
+            if message.startswith('Game over!') or len(message) == 0:
+                break
+
 
         
 
@@ -49,6 +65,12 @@ if __name__ == "__main__":
     server_data, server_addres = client.listening_for_requests()
 
     print(server_addres)
-    server_ip, server_tcp_port = server_addres[0], int(server_data[40:], 2)
+    server_ip, server_tcp_port = server_addres[0], server_data[2]
     
     client.initiate_tcp_connection(server_ip, server_tcp_port)
+
+
+
+    client.receive_message()
+
+
--- Server.py
+++ Server.py
@@ -1,15 +1,21 @@
 import socket
 import threading
+import random
+import struct
 from time import sleep
 
 class Server:
-    IP = socket.gethostbyname(socket.gethostname())
+    # IP = socket.gethostbyname(socket.gethostname())
+    IP = '172.18.0.61'
     UDP_PORT = 13117
     TCP_PORT = 50000
 
-    magic_cookie = b'11111110111011011011111011101111'
-    m_type = b'00000010'
-    server_port = b'1100001101010000'
+    # magic_cookie = b'11111110111011011011111011101111'
+    magic_cookie = 0xfeedbeef
+    # m_type = b'00000010'
+    m_type = 0x2
+    # server_port = b'1100001101010000'
+    server_port = 50000
     udp_offer = magic_cookie + m_type + server_port
 
     def __init__(self):
@@ -24,10 +30,9 @@ class Server:
         
         self.tcp_sock = socket.socket(socket.AF_INET, # Internet
                       socket.SOCK_STREAM) # TCP
-        self.tcp_sock.settimeout(1)
-
+        self.tcp_sock.settimeout(0.1)
+        self.tcp_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.tcp_sock.bind((self.IP, self.TCP_PORT))
-
 
         self.client_list = []
 
@@ -38,7 +43,8 @@ class Server:
 
         for i in range(10):
             print(i)
-            self.udp_sock.sendto(self.udp_offer, ('<broadcast>', self.UDP_PORT))
+            # self.udp_sock.sendto(self.udp_offer, ('<broadcast>', self.UDP_PORT))
+            self.udp_sock.sendto(struct.pack('IbH', self.magic_cookie, self.m_type, self.server_port), ('<broadcast>', self.UDP_PORT))
             sleep(1)
 
         self.sending_udp_messages = False
@@ -53,6 +59,7 @@ class Server:
                 client_thread.start()
 
             except socket.timeout:
+                # print("socket timed out")
                 continue
 
 
@@ -63,6 +70,47 @@ class Server:
 
         self.client_list.append((client_socket, client_address, team_name))
 
+    def assign_to_groups(self):
+        group_a, group_b = [], []
+        client_copy = list(self.client_list)
+        l = len(self.client_list)
+
+        for i in range(l//2):
+            r = random.randint(0, len(client_copy))
+
+            group_a.append(client_copy[r])
+            client_copy.pop(r)
+        
+        for client in client_copy:
+            group_b.append(client)
+
+        return group_a, group_b
+
+    def create_game_start_message(self, group_a, group_b):
+        m = 'Welcome to Keyboard Spamming Battle Royale.\n'
+        m += 'Group 1:\n==\n'
+        for c in group_a:
+            m += c[2]
+        
+        m += 'Group 2:\n==\n'
+        for c in group_b:
+            m += c[2]
+
+        m += 'Start pressing keys on your keyboard as fast as you can!!'
+
+        return m
+
+    def send_tcp_message(self, message):
+        l_thread = []
+        for client in self.client_list:
+            # client[0].send(bytes(message, 'utf-8'))
+            x = threading.Thread(client[0].send, args=(bytes(message, 'utf-8'),))
+            l_thread.append(x)
+            x.start()
+            
+        
+
+
 
 
 
@@ -70,15 +118,31 @@ if __name__ == "__main__":
     server = Server()
     # server.main()
     server.tcp_sock.listen()
-    udp_message_thread = threading.Thread(target=server.send_udp_message)
-    udp_message_thread.start()
-    
-    tcp_receive_thread = threading.Thread(target=server.accept_connections)
-    tcp_receive_thread.start()
+    while True:
+        udp_message_thread = threading.Thread(target=server.send_udp_message)
+        udp_message_thread.start()
+        
+        tcp_receive_thread = threading.Thread(target=server.accept_connections)
+        tcp_receive_thread.start()
 
-    udp_message_thread.join()
-    tcp_receive_thread.join()
+        udp_message_thread.join()
+        tcp_receive_thread.join()
 
-    print(server.client_list)
+        if len(server.client_list) < 2:
+            print("not enough players!")
+            continue
+
+        group_a, group_b = server.assign_to_groups()
+
+        print("group_a: {}".format(group_a))
+        print("group_b: {}".format(group_b))
+
+        game_start_message = server.create_game_start_message(group_a, group_b)
+        server.send_tcp_message(game_start_message)
+
+    # server.tcp_sock.shutdown(socket.SHUT_RDWR)
+    # server.tcp_sock.close()
+
+    # server.start_game()
 
     # server.send_udp_message()
